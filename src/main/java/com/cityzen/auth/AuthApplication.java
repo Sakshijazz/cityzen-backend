d4f470d0ad394c526f50b09943f6b2d896900b33
Sakshijazz <sakshijazzwal8248@gmail.com>
2025-07-29 01:42:20 +0530

    Set the environment variable for sensitive infos

--- src/main/java/com/cityzen/auth/AuthApplication.java
+++ src/main/java/com/cityzen/auth/AuthApplication.java
@@ -1,10 +1,11 @@
 package com.cityzen.auth;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
-
+        Dotenv dotenv = Dotenv.load();
         SpringApplication.run(AuthApplication.class, args);
         System.out.println("Server started");
     }
